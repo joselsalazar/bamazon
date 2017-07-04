e019c8f6521db336397345aca04918fadddbb2e2
Jose Salazar <jose.salazar.home@gmail.com>
2017-07-04 10:26:32 -0700

    Working Prototype 07/04

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,8 @@
 let inquirer = require('inquirer');
 let mySQL = require('mysql');
 
+let choicesArr = [];
+
 // MySQL connection
 let connection = mySQL.createConnection({
 	host: "localhost",
@@ -12,14 +14,86 @@ let connection = mySQL.createConnection({
 
 connection.connect(function(err) {
 	if(err) throw err;
-	displayItems();
+	startQuery();
 });
 
-function displayItems() {
-	connection.query("SELECT * FROM products", function(err, results) {
-		if (err) throw err;
-		for (let i = 0; i < results.length; i++) {
-			console.log(results[i].product_name);
+function startQuery() {
+	inquirer.prompt([
+		{
+			message: "What would you like to do?",
+			type: "list",
+			name: "startOptions",
+			choices: ["Buy", "Sell", "Exit"]
+		}
+	]).then(function(answer) {
+		if(answer.startOptions === "Buy") {
+			createArray();
+		} else if (answer.startOptions === "Sell") {
+			console.log("Feature Not Available Yet...");
+		} else {
+			process.exit();
 		}
 	});
 }
+
+function createArray() {
+	connection.query("SELECT * FROM products", function(err, results) {
+		if (err) throw err;
+		console.log("Current Items Available:");
+		for (let i = 0; i < results.length; i++) {
+			choicesArr.push(results[i]);
+
+			console.log(
+				"=====================================" + "\n" + 
+				"Item #:" + choicesArr[i].item_id + "\n" +
+				"Product:" + choicesArr[i].product_name + "\n" +
+				"Price: $" + choicesArr[i].price + "\n" +
+				"Current Stock:" + choicesArr[i].stock_quantity + "\n" +
+				"====================================="
+			);
+		}
+
+
+		inquirer.prompt([
+			{
+				message: "Which product would you like to buy?",
+				type: "list",
+				name: "products",
+				choices: function() {
+					var buyingChoices = [];
+					for (let i = 0; i < choicesArr.length; i++) {
+						buyingChoices.push(choicesArr[i].product_name);
+					}
+					return buyingChoices;
+				}	
+			},
+			{
+				message: "How many would you like to buy?",
+				name: "quantity",
+				validate: function(number) {
+					if(isNaN(number) === false) {
+						return true;
+					}
+					return false;
+				}
+			}
+		]).then(function(answer) {
+			// console.log(choicesArr[1].product_name);
+			var chosenItem;
+			for (var i = 0; i < choicesArr.length; i++) {
+				if(choicesArr[i].product_name === answer.products) {
+					chosenItem = choicesArr[i];
+				}
+			}
+
+			if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+				newStock = chosenItem.stock_quantity - parseInt(answer.quantity);
+				
+				console.log(newStock);
+			} else {
+				console.log("There is not enough!");
+			}
+		});
+	});
+}
+	
